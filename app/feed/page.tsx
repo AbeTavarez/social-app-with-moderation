a8f1af9d50eb203089d87d396d80c6719a8a9a59
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-06-30 23:47:39 -0400

    Feed Page handleSubmit completed

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,14 +1,24 @@
 import { Metadata } from "next";
+import { fetchPosts } from "@/actions";
+import CreatePostForm from "@/components/create-post-form";
 
 export const metadata: Metadata = {
-  title: 'Feed',
-  description: 'See posts from all over the world!'
-}
+  title: "Feed",
+  description: "See posts from all over the world!",
+};
 
-export default function Feed() {
+export default async function Feed() {
+  const posts = await fetchPosts();
+
+
+
   return (
     <div>
       <h1>Feed Page</h1>
+
+      <CreatePostForm />
+
+      {posts && posts.map(post => <div key={post.id}>{post.text}</div>)}
     </div>
   );
 }
